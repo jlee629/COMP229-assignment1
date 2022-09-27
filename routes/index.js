37d32e77d286456defe68ab180dfb88b9c7e71d3
Jungyu Lee <jlee629@my.centennialcollege.ca>
2022-09-27 11:31:04 -0400

    navbar complete

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,8 @@ router.get('/about', function(req, res, next) {
 });
 
 /* GET Products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products'});
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects'});
 });
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
