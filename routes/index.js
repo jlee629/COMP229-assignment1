d1cd485d1d36ff8c6e4c4c5e66dfa714de8c2a70
Jungyu Lee <jlee629@my.centennialcollege.ca>
2022-09-28 02:28:19 -0400

    Contact in progress

--- routes/index.js
+++ routes/index.js
@@ -18,15 +18,15 @@ router.get('/about', function(req, res, next) {
 
 /* GET Products page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects'});
+  res.render('projects', { title: 'My Projects'});
 });
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services'});
+  res.render('services', { title: 'My Services'});
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact'});
+  res.render('contact', { title: 'My Contact'});
 });
 module.exports = router;
